b3b6a7981ca822068ebc3bdf794d99e044f429a5
Guy Cole <guycole@gmail.com>
2026-07-30 05:19:35 +0000

    script refresh

--- bin/validator.sh
+++ bin/validator.sh
@@ -11,8 +11,9 @@ echo "start validate"
 #
 docker rm hyena-validate;docker run -v /var/wombat:/mnt/wombat --name hyena-validate hyena:latest
 #
-#docker rm hyena-koala;docker run -e stuntbox=koala -v /var/wombat:/mnt/wombat --name hyena-koala hyena:latest
-#$HOME/github/mellow-hyena-v2/bin/koala-import.sh
+docker rm hyena-koala;docker run -e stuntbox=koala -v /var/wombat:/mnt/wombat --name hyena-koala hyena:latest
+$HOME/github/mellow-hyena-v2/bin/koala-adsb.sh
+$HOME/github/mellow-hyena-v2/bin/koala-uat.sh
 #
 echo "end validate"
 #
