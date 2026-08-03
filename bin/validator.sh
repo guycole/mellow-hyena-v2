00d40f7e39a9a462dc78e5395abb41dc0c4ed13e
Guy Cole <guycole@gmail.com>
2026-08-03 03:08:02 +0000

    update for ghcr

--- bin/validator.sh
+++ bin/validator.sh
@@ -7,13 +7,19 @@
 #
 PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
 #
+CONTAINER1="wombat-hyena"
+CONTAINER2="koala-hyena"
+IMAGE="ghcr.io/guycole/wombat-hyena:latest"
+#
+WOMBAT_UID=$(id -u wombat)
+WOMBAT_GID=$(id -g wombat)
+#
 echo "start validate"
 #
-docker rm hyena-validate;docker run -v /var/wombat:/mnt/wombat --name hyena-validate hyena:latest
+docker rm ${CONTAINER1};docker run -e WOMBAT_UID=${WOMBAT_UID} -e WOMBAT_GID=${WOMBAT_GID} -v /var/wombat:/mnt/wombat --name ${CONTAINER1} ${IMAGE}
 #
-docker rm hyena-koala;docker run -e stuntbox=koala -v /var/wombat:/mnt/wombat --name hyena-koala hyena:latest
-$HOME/github/mellow-hyena-v2/bin/koala-adsb.sh
-$HOME/github/mellow-hyena-v2/bin/koala-uat.sh
+#
+docker rm ${CONTAINER2};docker run -e stuntbox=koala -v /var/wombat:/mnt/wombat --name ${CONTAINER2} ${IMAGE}
 #
 echo "end validate"
 #
